7fd5816c1570f1e2b3f7d25da2096dd1a002df73
Александр Кошелев <Koshelevsanya1994@yandex.ru>
2023-05-20 18:50:32 +0300

    Добавлен TODO

--- test_task/app.py
+++ test_task/app.py
@@ -16,13 +16,14 @@ df = pd.read_sql("SELECT * from sources", conn)
 color_dict = df[['reason', 'color']].set_index('reason').to_dict()['color']
 color_list_hex = [i for i in color_dict.values()]
 data_pie = df[['reason', 'duration_hour', 'color']]
+
+# TODO 'Настроить цвета круговой диаграммы'
 fig_pie = px.pie(
     data_frame=data_pie,
     values='duration_hour',
     names='reason',
     color_discrete_sequence = color_list_hex,
 )
-df_pie.show()
 
 
 
